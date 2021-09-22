c663f010e53ec5a62c862fe698e3a88d934cbeb7
Satwik Gawand <satwikgawand@gmail.com>
2021-09-22 19:02:00 +0530

    update: project done

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -5,15 +5,21 @@ import useGoogleSearch from '../components/useGoogleSearch';
 import Response from '../response';
 import { Link } from 'react-router-dom';
 import Search from '../components/Search';
+import SearchIcon from '@material-ui/icons/Search';
+import DescriptionIcon from '@material-ui/icons/Description';
+import ImageIcon from '@material-ui/icons/Image';
+import LocalOfferIcon from '@material-ui/icons/LocalOffer';
+import RoomIcon from '@material-ui/icons/Room';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 function SearchPage() {
   const [{ term }, dispatch] = useStateValue();
   
   // Live API Call
-  // const { data } = useGoogleSearch(term);
+  const { data } = useGoogleSearch(term);
 
   // Local Data
-  const data = Response;
+  // const data = Response;
 
   console.log(data);
 
@@ -25,11 +31,69 @@ function SearchPage() {
         </Link>
         <div className='searchPage__headerBody'>
           <Search hideButtons />
+          <div className='searchPage__options'>
+            <div className='searchPage__optionsLeft'>
+              <div className='searchPage__option'>
+                <SearchIcon />
+                <Link to='/all'>All</Link>
+              </div>
+              <div className='searchPage__option'>
+                <DescriptionIcon />
+                <Link to='/news'>News</Link>
+              </div>
+              <div className='searchPage__option'>
+                <ImageIcon />
+                <Link to='/images'>Images</Link>
+              </div>
+              <div className='searchPage__option'>
+                <LocalOfferIcon />
+                <Link to='/shopping'>Shopping</Link>
+              </div>
+              <div className='searchPage__option'>
+                <RoomIcon />
+                <Link to='/maps'>Maps</Link>
+              </div>
+              <div className='searchPage__option'>
+                <MoreVertIcon />
+                <Link to='/more'>More</Link>
+              </div>
+            </div>
+            <div className='searchPage__optionsRight'>
+              <div className='searchPage__option'>
+                <Link to='/settings'>Settings</Link>
+              </div>
+              <div className='searchPage__option'>
+                <Link to='/tools'>Tools</Link>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      <div className='searchPage__results'>
+      {true && (
+        <div className='searchPage__results'>
+          <p className='searchPage__resultCount'>
+            About {data?.searchInformation.formattedTotalResults} results ({data.searchInformation.formattedSearchTime} seconds) for {term}
+          </p>
 
-      </div>
+          {data?.items.map(item => (
+            <div className='searchPage__result'>
+              <a className='searchPage__resultLink' href={item.link}>
+                {item.pagemap?.cse_image?.length > 0 && item.pagemap?.cse_image[0]?.src && (
+                  <img 
+                    className='searchPage__resultImage' src={item.pagemap?.cse_image[0]?.src} alt=''
+                  />
+                )}
+
+                {item.displayLink}
+              </a>
+              <a className='searchPage__resultTitle' href={item.link}>
+                <h2>{item.title}</h2>
+              </a>
+              <p className='searchPage__resultSnippet'>{item.snippet}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   )
 }
